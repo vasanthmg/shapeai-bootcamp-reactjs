f0d2e9106bd8cc53e97415126d7ddc1d9d29fe6e
vasanthmg <84905428+vasanthmg@users.noreply.github.com>
2021-06-05 17:43:57 +0530

    Add files via upload

---
+++ app.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./header";
+import Footer from "./footer";
+import Note from "./note";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+}
+
+export default App;
